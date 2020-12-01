bb5e87a8b9893d5613862e6b0971cdd0814a2fc6
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-01 11:25:23 +0900

    簡単ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲストユーザー"
+      user.password = SecureRandom.urlsafe_base64
+    end
+    
+  end
 end
