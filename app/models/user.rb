274ccc87dde0e4ab60c9e04717cca2852bd28803
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-02 19:49:18 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
 
+  password_vali = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: password_vali, message: 'should include English and numbers,and can be used only in half-width'}
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.name = "ゲストユーザー"
       user.password = SecureRandom.urlsafe_base64
     end
-    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  
+  describe 'ユーザー新規登録' do
+
+    context '新規登録できる時' do
+      it '全てのカラムが入っていれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない時' do
+      it '名前が空では保存できない' do
+        @user.name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Name can't be blank"
+      end
+
+      it 'emailが空では保存できない' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
+
+      it 'emailは一意性である' do
+        @user.save
+        other_user = FactoryBot.build(:user)
+        other_user.email = @user.email
+        other_user.valid?
+        expect(other_user.errors.full_messages).to include "Email has already been taken"
+      end
+
+      it 'emailは＠がなければ保存できない' do
+        @user.email = "aaaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
+
+      it 'passwordが空では保存できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+
+      it 'passwordは全角を使用できない' do
+        @user.password = "あああああ１２"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password should include English and numbers,and can be used only in half-width"
+      end
+
+      it 'passwordは6文字以上でなければならない' do
+        @user.password = "aa123"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      end
+
+      it 'passwordは半角英数字混合でなければならない' do
+        @user.password = "aaaaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password should include English and numbers,and can be used only in half-width"
+      end
+
+    end
+  end
 end
