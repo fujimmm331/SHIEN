89e539ec4317f7b304d406714b258e71a339817e
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-06 20:05:11 +0900

    コントローラーの設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,23 +5,4 @@ class TeamsController < ApplicationController
   def new
     @team = Team.new
   end
-
-  def create
-    @team = Team.new(team_params)
-
-    if @team.valid?
-      @team.save
-      redirect_to root_path
-    else
-      render :new 
-    end
-  end
-
-
-  private
-
-  def team_params
-    params.require(:team).permit(:name).merge(user_id: current_user.id)
-  end
-
 end
