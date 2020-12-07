7d2f327b1fd4081fb052dbbca3a6395920a35fc8
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-07 22:00:27 +0900

    中間テーブルを再度作成

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -1,4 +1,4 @@
 class TeamUser < ApplicationRecord
-  belongs_to :team
   belongs_to :user
+  belongs_to :team
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_130558) do
+ActiveRecord::Schema.define(version: 2020_12_06_111431) do
 
   create_table "team_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "team_id"
