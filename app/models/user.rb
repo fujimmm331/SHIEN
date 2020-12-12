52325114b6e7ca7b2a259e22dc1123802e82e60e
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-12 13:08:34 +0900

    外部キーを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.name = "ゲストユーザー"
       user.password = SecureRandom.urlsafe_base64
+      user.team_id = 1
     end
   end
 end
