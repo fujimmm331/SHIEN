c3fde4c3fc58a21fd56f858431b02fe4a30760ab
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-13 18:19:00 +0900

    一意性制約を実装

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,5 @@ class Team < ApplicationRecord
   has_many :users
   has_many :matters, through: :team_matters
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
