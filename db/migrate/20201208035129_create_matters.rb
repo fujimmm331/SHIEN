b30bc8259855e925fc6605ca2084db8be6a1e7e1
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-15 15:05:22 +0900

    マイグレーションファイルの設定

--- db/migrate/20201208035129_create_matters.rb
+++ db/migrate/20201208035129_create_matters.rb
@@ -1,7 +1,17 @@
 class CreateMatters < ActiveRecord::Migration[6.0]
   def change
     create_table :matters do |t|
-      t.string :name, null: false
+      t.string :name,                null: false
+      t.string :sales_person,        null: false
+      t.string :kana_sales_person,   null: false
+      t.integer :phone_number,       null: false
+      t.integer :cell_phone_number,  null: false
+      t.string :postal_code
+      t.string :municipality,        null: false
+      t.string :address,             null: false
+      t.string :building,            null: false
+      t.references :user,            foreign_key: true
+      t.references :team,            foreign_key: true
       t.timestamps
     end
   end
