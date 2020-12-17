c36085676d7e93cc95f41cbf854116fe3b39ebbf
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-17 21:12:24 +0900

    matterモデル単体テスト実装

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'capistrano3-unicorn'
   gem 'ed25519'
   gem 'bcrypt_pbkdf'
+  gem 'gimei'
 end
 
 group :development do
--- spec/factories/matters.rb
+++ spec/factories/matters.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
+  gimei = Gimei.name
+  sales_name = gimei.kanji.gsub(" ","")
+  kana_sales_name = gimei.katakana.gsub(" ","")
+  address = Gimei.address
+
   factory :matter do
-    
+    name {"テスト案件"}
+    sales_person {sales_name} 
+    kana_sales_person {kana_sales_name}
+    phone_number {"0311112222"}
+    cell_phone_number {"09011113333"}
+    postal_code {"111-1111"}
+    municipality {address.city.kanji}
+    address {"1-1-1"}
+    building {"フレーム神南坂4F"}
+    association :user
+    association :team
   end
 end
