9f9f7371c19dec5476cd569558310004a13fd2ab
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-17 12:19:03 +0900

    ログインユーザーのみ案件を見られる様に設定

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,5 +1,13 @@
 class MattersController < ApplicationController
+  before_action :sign_in_check
   def index
     @matters = Matter.all
+  end
+
+  private
+  def sign_in_check
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
