191449a2c3b94951010aec03de9d9b50099e5f38
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-17 17:33:41 +0900

    案件作成のバックサイド実装

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -9,7 +9,21 @@ class MattersController < ApplicationController
     @matter = Matter.new
   end
   
+  def create
+    @matter = Matter.new(matter_params)
+    if @matter.valid?
+      @matter.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
+  def matter_params
+    params.require(:matter).permit(:name, :sales_person, :kana_sales_person, :phone_number, :cell_phone_number, :postal_code, :municipality, :address, :building).merge(user_id: current_user.id, team_id:current_user.team.id)
+  end
+
   def sign_in_check
     unless user_signed_in?
       redirect_to root_path
