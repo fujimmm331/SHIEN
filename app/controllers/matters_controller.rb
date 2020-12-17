b1dcf73594dc762bbf9f33d7a0fee97e92a91714
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-17 15:09:24 +0900

    新規案件作成ページへ遷移できるように設定

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -5,6 +5,10 @@ class MattersController < ApplicationController
     @matters = Matter.all.order(id: "DESC")
   end
 
+  def new
+    @matter = Matter.new
+  end
+  
   private
   def sign_in_check
     unless user_signed_in?
