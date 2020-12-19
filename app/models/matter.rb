f90544b7e659413e236854acd4f1b37c44fd0ad1
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-19 17:02:04 +0900

    fix: バリデーション不要のため、記述を削除

--- app/models/matter.rb
+++ app/models/matter.rb
@@ -3,7 +3,7 @@ class Matter < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    validates :name, :sales_person, :municipality, :address, :building
+    validates :name, :sales_person, :municipality, :address
     validates :kana_sales_person, format:{ with:/\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :phone_number, :cell_phone_number, format:{ with:/\A\d{10,11}\z/, message: "can use only half-width numbers." }
     validates :postal_code, format:{ with:/\A\d{3}[-]\d{4}\z/, message: "should include hyphen, and can use only half-width numbers."}
