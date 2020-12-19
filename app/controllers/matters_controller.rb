953209e34e8d046b7686591e2bac22bd03de5b47
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-19 17:00:39 +0900

    feat: 案件編集機能を実装

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,5 +1,6 @@
 class MattersController < ApplicationController
   before_action :sign_in_check
+  before_action :find_matter, only: [:show, :edit, :update]
   
   def index
     @matters = Matter.all.order(id: "DESC")
@@ -20,7 +21,18 @@ class MattersController < ApplicationController
   end
 
   def show
-    @matter = Matter.find(params[:id])
+  end
+
+  def edit  
+  end
+
+  def update
+    if @matter.valid?
+      @matter.update(matter_params)
+      redirect_to matter_path(@matter.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -32,5 +44,9 @@ class MattersController < ApplicationController
     unless user_signed_in?
       redirect_to root_path
     end
+  end
+
+  def find_matter
+    @matter = Matter.find(params[:id])
   end
 end
