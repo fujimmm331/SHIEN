f8b0cc8bc1d02adb81e09d19410fc3fa88e2a66e
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-22 17:48:50 +0900

    fix:バリデーションを修正に伴い、seedの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 end
 
 10.times do |i|
-  Matter.create(name:"案件#{i + 1}", sales_person:"田中太郎", kana_sales_person:"タナカタロウ", phone_number:"0399991111", cell_phone_number:"09099992222", postal_code:"249-0006", municipality:"逗子市逗子市", address:"4-5-14", building:"藤村ビル1F", user_id:1, team_id:1)
+  Matter.create(name:"案件#{i + 1}", sales_person:"田中太郎", kana_sales_person:"タナカタロウ", phone_number:"0399991111", cell_phone_number:"09099992222", postal_code:"2490006", municipality:"逗子市逗子市", address:"4-5-14", building:"藤村ビル1F", user_id:1, team_id:1)
 end
