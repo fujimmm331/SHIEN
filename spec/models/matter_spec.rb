e52316db1019b914173c7cb852cb30ab6f750ae5
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-22 15:30:38 +0900

    test:エラー日本語化に伴い、matterモデルを再テスト

--- spec/models/matter_spec.rb
+++ spec/models/matter_spec.rb
@@ -23,121 +23,121 @@ RSpec.describe Matter, type: :model do
       it '案件名が空では保存できない' do
         @matter.name = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Name can't be blank"
+        expect(@matter.errors.full_messages).to include "案件名を入力してください"
       end
 
       it '先方の担当者が空では保存できない' do
         @matter.sales_person = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Sales person can't be blank"
+        expect(@matter.errors.full_messages).to include "先方の担当者を入力してください"
       end
 
       it 'フリガナが空では保存できない' do
         @matter.kana_sales_person = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Kana sales person can't be blank"
+        expect(@matter.errors.full_messages).to include "フリガナを入力してください"
       end
 
       it 'フリガナはカタカナ以外を保存できない' do
         @matter.kana_sales_person = "あああああ"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Kana sales person is invalid. Input full-width katakana characters."
+        expect(@matter.errors.full_messages).to include "フリガナは全角カタカナのみ使用できます"
       end
 
       it 'フリガナは半角カタカナを保存できない' do
         @matter.kana_sales_person = "ｱｱｱｱ"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Kana sales person is invalid. Input full-width katakana characters."
+        expect(@matter.errors.full_messages).to include "フリガナは全角カタカナのみ使用できます"
       end
 
       it '電話番号が空では保存できない' do
         @matter.phone_number = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can't be blank"
+        expect(@matter.errors.full_messages).to include "電話番号を入力してください"
       end
 
       it '電話番号は数字以外を保存できない' do
         @matter.phone_number = "あいうえお"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "電話番号は半角数字・11桁以内でお願いします"
       end
 
       it '電話番号は12桁以上を保存できない' do
         @matter.phone_number = "030000000000"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "電話番号は半角数字・11桁以内でお願いします"
       end
 
       it '電話番号は全角数字を保存できない' do
         @matter.phone_number = "０３１１１１２２２２"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "電話番号は半角数字・11桁以内でお願いします"
       end
 
       it '携帯電話番号が空では保存できない' do
         @matter.cell_phone_number = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Cell phone number can't be blank"
+        expect(@matter.errors.full_messages).to include "携帯電話番号を入力してください"
       end
 
       it '携帯電話番号は12桁以上を保存できない' do
-        @matter.phone_number = "090000000000"
+        @matter.cell_phone_number = "090000000000"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "携帯電話番号は半角数字・11桁以内でお願いします"
       end
 
       it '携帯電話番号は全角数字を保存できない' do
-        @matter.phone_number = "０９０１１１１２２２２"
+        @matter.cell_phone_number = "０９０１１１１２２２２"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Phone number can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "携帯電話番号は半角数字・11桁以内でお願いします"
       end
 
       it '郵便番号が空では保存できない' do
         @matter.postal_code = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Postal code can't be blank"
+        expect(@matter.errors.full_messages).to include "郵便番号を入力してください"
       end
 
       it '郵便番号は数字以外を保存できない' do
-        @matter.postal_code = "あああ-ああああ"
+        @matter.postal_code = "あああああああ"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Postal code should include hyphen, and can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "郵便番号は半角数字・7桁以内でお願いします"
       end
 
       it '郵便番号は全角数字を保存できない' do
-        @matter.postal_code = "１２３-４５６７"
+        @matter.postal_code = "１２３４５６７"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Postal code should include hyphen, and can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "郵便番号は半角数字・7桁以内でお願いします"
       end
 
-      it 'ハイフン抜きでは郵便番号を保存できない' do
-        @matter.postal_code = "1234567"
+      it '郵便番号を8桁以上を保存できない' do
+        @matter.postal_code = "12345678"
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Postal code should include hyphen, and can use only half-width numbers."
+        expect(@matter.errors.full_messages).to include "郵便番号は半角数字・7桁以内でお願いします"
       end
 
       it '市区町村が空では保存できない' do
         @matter.municipality = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Municipality can't be blank"
+        expect(@matter.errors.full_messages).to include "市区町村を入力してください"
       end
 
       it '番地が空では保存できない' do
         @matter.address = ""
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Address can't be blank"
+        expect(@matter.errors.full_messages).to include "番地を入力してください"
       end
 
       it 'ユーザーが空では保存できない' do
         @matter.user = nil
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "User must exist"
+        expect(@matter.errors.full_messages).to include "ユーザーを入力してください"
       end
 
       it '番地が空では保存できない' do
         @matter.team = nil
         @matter.valid?
-        expect(@matter.errors.full_messages).to include "Team must exist"
+        expect(@matter.errors.full_messages).to include "所属を入力してください"
       end
     end
   end
