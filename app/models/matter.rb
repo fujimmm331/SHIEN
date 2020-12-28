19ef820ada46fb194fa331ea7c3c7e7b49e113f1
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-12-28 19:19:57 +0900

    fix:email追加のため、バリデーションを追加

--- app/models/matter.rb
+++ app/models/matter.rb
@@ -3,11 +3,11 @@ class Matter < ApplicationRecord
   belongs_to :team
   belongs_to :user
 
-  with_options presence: true do
-    validates :name, :sales_person, :municipality, :address
-    validates :kana_sales_person, format:{ with:/\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみ使用できます"}
-    validates :email, format:{ with:/\A\S+@\S+\.\S+\z/, message: "が間違っています。"}
-    validates :phone_number, :cell_phone_number, format:{ with:/\A\d{10,11}\z/, message: "は半角数字・11桁以内でお願いします" }
-    validates :postal_code, format:{ with:/\A\d{7}\z/, message: "は半角数字・7桁以内でお願いします"}
-  end
+  
+  validates :name, :sales_person, :kana_sales_person, :email, :phone_number, :cell_phone_number, :postal_code, :municipality, :address, presence: true
+  validates :kana_sales_person, format:{ with:/\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみ使用できます"}, unless: Proc.new { |a| a.kana_sales_person.blank? }
+  validates :email, format:{ with:/\A\S+@\S+\.\S+\z/, message: "が間違っています。"}, unless: Proc.new { |a| a.email.blank? }
+  validates :phone_number, :cell_phone_number, format:{ with:/\A\d{10,11}\z/, message: "は半角数字・11桁以内でお願いします" }, unless: Proc.new { |a| a.phone_number.blank? }
+  validates :postal_code, format:{ with:/\A\d{7}\z/, message: "は半角数字・7桁以内でお願いします"}, unless: Proc.new { |a| a.postal_code.blank? }
+
 end
