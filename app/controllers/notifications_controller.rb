984574898142c5a1dd63d1dff86b1df88abdaf18
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-01-02 11:05:57 +0900

    fix:通知の一覧表示を雛形に合わせて修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
   def index
+    @notifications = Notification.where(visited_id: current_user.id).where.not(visiter_id: current_user.id).order(created_at: :desc)
   end
 end
