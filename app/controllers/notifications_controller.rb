743ea579f6c2f299ec0177e65f022f27ca5ce7d5
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-01-03 10:24:33 +0900

    feat:どのページにいても、通知が何件あるかわかるように設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,4 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = Notification.where(visited_id: current_user.id).where.not(visiter_id: current_user.id).order(created_at: :desc)
   end
 end
