26af184d4ab6db304e22d173fedde9cde2971071
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-01-11 17:45:15 +0900

    fix:検索機能を試せるように、電話番号をランダム化

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@ User.create(name:"ゲストユーザー", email:"guest@example.com", password:Se
 end
 
 
+
 100.times do |i|
   gimei = Gimei.name
   sales_name = gimei.kanji.gsub(" ","")
   kana_sales_name = gimei.katakana.gsub(" ","")
   email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
-  
-  Matter.create(name:"サンプル案件#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:"0312345678", cell_phone_number:"09087654321", postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:1, team_id:1)
+
+  cell_phone_number = "0900000#{rand(1000..9999)}"
+  phone_number = "030000#{rand(1000..9999)}"
+
+  Matter.create(name:"サンプル案件#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:1, team_id:1)
 end
 
 100.times do |i|
@@ -22,8 +26,10 @@ end
   kana_sales_name = gimei.katakana.gsub(" ","")
   email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
+  cell_phone_number = "0900000#{rand(1000..9999)}"
+  phone_number = "030000#{rand(1000..9999)}"
   
-  Matter.create(name:"サンプル商事#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:"0312345678", cell_phone_number:"09087654321", postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:2, team_id:1)
+  Matter.create(name:"サンプル商事#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:2, team_id:1)
 end
 
 100.times do |i|
@@ -32,8 +38,10 @@ end
   kana_sales_name = gimei.katakana.gsub(" ","")
   email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
+  cell_phone_number = "0900000#{rand(1000..9999)}"
+  phone_number = "030000#{rand(1000..9999)}"
   
-  Matter.create(name:"株式会社サンプル#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:"0312345678", cell_phone_number:"09087654321", postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:3, team_id:1)
+  Matter.create(name:"株式会社サンプル#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:3, team_id:1)
 end
 
 100.times do |i|
@@ -42,6 +50,8 @@ end
   kana_sales_name = gimei.katakana.gsub(" ","")
   email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
+  cell_phone_number = "0900000#{rand(1000..9999)}"
+  phone_number = "030000#{rand(1000..9999)}"
   
-  Matter.create(name:"有限会社サンプル#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:"0312345678", cell_phone_number:"09087654321", postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:4, team_id:1)
+  Matter.create(name:"有限会社サンプル#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:4, team_id:1)
 end
