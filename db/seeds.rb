620ee771001c6c84ebc76099fd02637275625d9a
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-01-23 12:14:03 +0900

    ref:ダミーアドレスと分かりにくかったため修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,20 @@ User.create(name:"ゲストユーザー2", email:"guest2@example.com", password:
   gimei = Gimei.name
   sales_name = gimei.kanji.gsub(" ","")
   kana_sales_name = gimei.katakana.gsub(" ","")
-  email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
-
   cell_phone_number = "0900000#{rand(1000..9999)}"
   phone_number = "030000#{rand(1000..9999)}"
 
-  Matter.create(name:"サンプル案件#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:1, team_id:1)
+  Matter.create(name:"サンプル案件#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: "sample1@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:1, team_id:1)
 end
 
 200.times do |i|
   gimei = Gimei.name
   sales_name = gimei.kanji.gsub(" ","")
   kana_sales_name = gimei.katakana.gsub(" ","")
-  email = Faker::Internet.free_email
   municipality = Gimei.address.city.kanji
   cell_phone_number = "0900000#{rand(1000..9999)}"
   phone_number = "030000#{rand(1000..9999)}"
   
-  Matter.create(name:"サンプル商事#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: email, phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:2, team_id:1)
+  Matter.create(name:"サンプル商事#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: "sample2@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:2, team_id:1)
 end
