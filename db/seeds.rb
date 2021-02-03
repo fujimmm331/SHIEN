7a04b300ca623267182002f532395c083478c2bd
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-03 19:57:39 +0900

    fix:モデル名変更に伴い、seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,24 @@ User.create(name:"開発者アカウント", email:"fujimura@kazuya.com", passwo
 
 200.times do |i|
   gimei = Gimei.name
-  sales_name = gimei.kanji.gsub(" ","")
-  kana_sales_name = gimei.katakana.gsub(" ","")
-  municipality = Gimei.address.city.kanji
+  customer_name = "サンプル" + gimei.first.kanji
+  kana_customer_name = "サンプル" + gimei.first.katakana
+  municipality = Gimei.address.kanji
   cell_phone_number = "0900000#{rand(1000..9999)}"
   phone_number = "030000#{rand(1000..9999)}"
+  hobbies = %w(スノーボード ゴルフ 釣り サッカー フットサル 映画鑑賞 カメラ サーフィン ツーリング ガーデニング お菓子作り カフェ巡り)
 
-  Matter.create(name:"サンプル案件#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: "sample1@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:1, team_id:1)
+  Customer.create(name: customer_name, kana_name: kana_customer_name, email: "sample1@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", hobby: hobbies[rand(0..(hobbies.count - 1))], memo:"これはサンプルです。", user_id:1, team_id:1)
 end
 
 200.times do |i|
   gimei = Gimei.name
-  sales_name = gimei.kanji.gsub(" ","")
-  kana_sales_name = gimei.katakana.gsub(" ","")
-  municipality = Gimei.address.city.kanji
+  customer_name = "サンプル" + gimei.first.kanji
+  kana_customer_name = "サンプル" + gimei.first.katakana
+  municipality = Gimei.address.kanji
   cell_phone_number = "0900000#{rand(1000..9999)}"
   phone_number = "030000#{rand(1000..9999)}"
-  
-  Matter.create(name:"サンプル商事#{i + 1}", sales_person: sales_name, kana_sales_person: kana_sales_name, email: "sample2@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", user_id:2, team_id:1)
+  hobbies = %w(スノーボード ゴルフ 釣り サッカー フットサル 映画鑑賞 カメラ サーフィン ツーリング ガーデニング お菓子作り カフェ巡り)
+
+  Customer.create(name: customer_name, kana_name: kana_customer_name, email: "sample2@sample.com", phone_number:phone_number, cell_phone_number:cell_phone_number, postal_code:"1000001", municipality: municipality, address:"1-1-1", building:"サンプルビル1F", hobby: hobbies[rand(0..(hobbies.count - 1))], memo:"これはサンプルです。", user_id:2, team_id:1)
 end
