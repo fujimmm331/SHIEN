c7ecd73d9364e8760e39adc4e9dce301435d899d
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-06 23:43:42 +0900

    feat:自動車管理のためテーブル作成

--- db/migrate/20210202035603_create_cars.rb
+++ db/migrate/20210202035603_create_cars.rb
@@ -1,8 +1,26 @@
 class CreateCars < ActiveRecord::Migration[6.0]
   def change
     create_table :cars do |t|
-
+      t.string :name
+      t.string :vehicle_number
+      t.string :transport_bureau
+      t.integer :class_number
+      t.string :registration_type
+      t.string :registration_number
+      t.integer :vhicle_inspection_day
+      t.integer :registered_year
+      t.references :customer
       t.timestamps
     end
   end
 end
+
+
+# name                  => 車名
+# vehicle_number        => 車体番号
+# transport_bureau      => 運輸局（例 横浜、湘南）
+# class_number          => 分類番号（例 500 301）
+# registration_type     => 登録種別（ひらがなのところ）
+# registration_number   => 登録番号（4桁以内の数字）
+# vhicle_inspection_day => 車検満了日
+# registered_year       => 初度登録
