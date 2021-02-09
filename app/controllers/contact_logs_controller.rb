2bfe5025d55cc93b8ede29fc91a63214ca30bbbe
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-09 10:17:15 +0900

    fix:コンタクトログの表示・作成ができなかったため、修正

--- app/controllers/contact_logs_controller.rb
+++ app/controllers/contact_logs_controller.rb
@@ -1,19 +1,19 @@
 class ContactLogsController < ApplicationController
   def index
-    @matter = Matter.find(params[:matter_id])
-    @logs = @matter.contact_logs.order(created_at: :desc).includes(:user)
+    @customer = Customer.find(params[:customer_id])
+    @logs = @customer.contact_logs.order(created_at: :desc).includes(:user)
   end
 
   def create
     @contact_log = ContactLog.new(contact_log_params)
     @contact_log.save if @contact_log.valid?
-    @contact_log.notification_create(@contact_log.user.id, @contact_log.matter.user.id, @contact_log.matter.id, @contact_log.id)
-    redirect_to matter_path(params[:matter_id])
+    @contact_log.notification_create(@contact_log.user.id, @contact_log.customer.user.id, @contact_log.customer.id, @contact_log.id)
+    redirect_to customer_path(params[:customer_id])
   end
 end
 
 private
 
 def contact_log_params
-  params.require(:contact_log).permit(:status, :comment).merge(user_id: current_user.id, team_id:current_user.team.id, matter_id: params[:matter_id])
+  params.require(:contact_log).permit(:status, :comment).merge(user_id: current_user.id, team_id:current_user.team.id, customer_id: params[:customer_id])
 end
