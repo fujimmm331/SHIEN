36a2e1dcc1d380b66e4bf08f13986aa54dd0dd1b
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-09 20:04:42 +0900

    fix:扱いがわかりにくくなっていたため、修正

--- app/javascript/allClear.js
+++ app/javascript/allClear.js
@@ -1,9 +1,42 @@
 function allClear() {
-  const clearBtn = document.getElementById("clear_btn")
-  const checkBoxes = document.querySelectorAll(".checkbox")
-  clearBtn.addEventListener("click",function () {
-    checkBoxes.forEach(function (checkBox) {
-      checkBox.checked = false
+  const clearBtnForRecordsFalse = document.getElementById("clear_btn_for_records_false")
+  const clearBtnForRecordsTrue = document.getElementById("clear_btn_for_records_true")
+  const clearBtnForColumnsFalse = document.getElementById("clear_btn_for_columns_false")
+  const clearBtnForColumnsTrue = document.getElementById("clear_btn_for_columns_true")
+  const checkBoxesForRecords = document.querySelectorAll(".checkbox_for_record")
+  const checkBoxesForColumns = document.querySelectorAll(".checkbox_for_columns")
+  
+  clearBtnForRecordsFalse.addEventListener("click",function () {
+    checkBoxesForRecords.forEach(function (checkBox) {
+      checkBox.checked = false;
+      clearBtnForRecordsFalse.setAttribute("style", "display:none;")
+      clearBtnForRecordsTrue.removeAttribute("style", "display:none;")
+    });
+  });
+
+  clearBtnForRecordsTrue.addEventListener("click",function () {
+    checkBoxesForRecords.forEach(function (checkBox) {
+      checkBox.checked = true;
+      clearBtnForRecordsTrue.setAttribute("style", "display:none;")
+      clearBtnForRecordsFalse.removeAttribute("style", "display:none;")
+    });
+  });
+
+  
+
+  clearBtnForColumnsFalse.addEventListener("click",function () {
+    checkBoxesForColumns.forEach(function (checkBox) {
+      checkBox.checked = false;
+      clearBtnForColumnsFalse.setAttribute("style", "display:none;")
+      clearBtnForColumnsTrue.removeAttribute("style", "display:none;")
+    });
+  });
+
+  clearBtnForColumnsTrue.addEventListener("click",function () {
+    checkBoxesForColumns.forEach(function (checkBox) {
+      checkBox.checked = true;
+      clearBtnForColumnsTrue.setAttribute("style", "display:none;")
+      clearBtnForColumnsFalse.removeAttribute("style", "display:none;")
     });
   });
 
