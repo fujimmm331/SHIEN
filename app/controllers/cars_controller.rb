2c3f9c012329ed4f1d9e652bfe83c301df749b5c
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-11 22:25:07 +0900

    feat:車両情報が作成されるよう実装

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,20 @@ class CarsController < ApplicationController
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.new(car_params)
+    binding.pry
+    if @car.valid?
+      @car.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:name, :vehicle_number, :transport_bureau, :class_number, :registration_type, :registration_number, :vehicle_inspection_day, :registered_year, :customer_id)
+  end
 end
