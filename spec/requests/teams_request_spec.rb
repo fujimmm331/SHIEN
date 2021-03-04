2b34e91d6bf84b727d690ee7143564cc134f58f5
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-03-04 14:57:26 +0900

    test:teamsコントローラーテストの準備

--- spec/requests/teams_request_spec.rb
+++ spec/requests/teams_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Teams", type: :request do
+  describe '#index' do
+    context 'ログイン前' do
 
+    end
+
+    context 'ログイン後' do
+      
+    end
+  end
+
+  describe '#new' do
+    context 'ログイン前' do
+
+    end
+
+    context 'ログイン後' do
+      
+    end
+  end
+
+  describe '#create' do
+    context 'ログイン前' do
+
+    end
+
+    context 'ログイン後' do
+      
+    end
+  end
 end
